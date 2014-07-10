546c9862bdd5801e1d6abdaeb50e833cf13fc723
yfan1124 <yfan1124@hotmail.com>
2014-07-10 17:29:35 +0800

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,21 @@
+setwd("C:/Users/y2l/Dropbox/Coursera/Exploratory Data Analysis/Project 1")
+data <- read.table("household_power_consumption.txt", sep=";", header = TRUE, stringsAsFactors = FALSE, na.strings = "?")
+z = paste(data[,1],data[,2],sep = ' ')
+DateTime <- strptime(z, format = '%d/%m/%Y %H:%M:%S')
+data <- cbind(data, DateTime)
+
+x <- as.Date(z, format = '%d/%m/%Y %H:%M:%S')
+data[,1] <- x
+datasubset <- subset(data, data$Date == "2007-02-01")
+datasubset <- rbind(datasubset, subset(data, data$Date == "2007-02-02"))
+
+png("plot4.png", width=480, height=480)
+par(mfrow=c(2,2))
+plot(datasubset$DateTime, as.numeric(datasubset$Global_active_power), type="l", ylab = "Global Active Power", xlab = "")
+plot(datasubset$DateTime, as.numeric(datasubset$Voltage), type="l", xlab = "datetime", ylab ="Voltage")
+with(datasubset, plot(datasubset$DateTime, as.numeric(datasubset$Sub_metering_1), col = "black", type = "l", xlab = "", ylab = "Energy sub metering"))
+lines(datasubset$DateTime, as.numeric(datasubset$Sub_metering_2), col = "red")
+lines(datasubset$DateTime, as.numeric(datasubset$Sub_metering_3), col = "blue")
+legend("topright", col = c("black", "red", "blue"), lty= "solid", bty="n", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3") )
+plot(datasubset$DateTime, as.numeric(datasubset$Global_reactive_power), type="l", xlab = "datetime", ylab = "Global_reactive_power")
+dev.off()
